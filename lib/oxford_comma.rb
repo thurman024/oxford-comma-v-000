c86fe499628f713bc8f6c25210c644fb6c0797c1
Stephen Thurman <thurman024@gmail.com>
2018-01-24 21:07:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,15 +6,15 @@ def oxford_comma(array)
     return array[0] + " and " + array[1]
   else
     separators = Array.new(l, ", ") #create array with a comma for each item in array
+    last_item = array.last #save last item without a comma attached
     output_string = [] #define blank array to compile string into
     array.each_with_index do |entry, index|
-      output_string << entry + separators[index] #combine entry with comma
+      output_string << entry + separators[index] #combine each entry with comma
     end
 
-    # last two entries of output_string are "string" and ", "
-    # move this string to the end, and replace second to last entry of output_string with "and "
+    # last entry of output_string must be replaced with two entries - "and ", "last_item"
     l_2 = output_string.size
-    output_string[l_2] = output_string[l_2 - 1]
+    output_string[l_2] = last_item
     output_string[l_2 - 1] = "and "
     output_string.join
   end
